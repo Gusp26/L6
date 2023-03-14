008a62435facabb0616acf5ad29f423926323d36
pan da da <125976460+Gusp26@users.noreply.github.com>
2023-03-14 13:55:57 +0000

    Create a dummy endpoint with pervious knowledge

--- models/users.ts
+++ models/users.ts
@@ -1,8 +1,9 @@
 import * as db from '../helpers/database';
 
-export const findByUsername = async (username) => {
+export const findByUsername = async (username:string) => {
   const query = 'SELECT * from users where username = ?';
-  const user = await db.run_query(query, [username]);
+  let user_array = [username];
+  const user = await db.run_query(query, user_array);
   return user;
 }
 
